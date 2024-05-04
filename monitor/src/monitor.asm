9b93e08cb36e3a4adf7c10dee9aec64b98548429
Ferry Hendrikx <ferry.hendrikx@avenir-technology.com>
2024-05-04 18:14:46 +1200

    reworked the monitor

--- monitor/src/monitor.asm
+++ monitor/src/monitor.asm
@@ -13,6 +13,7 @@ org 0
 ;======================================================================
 
 %include "macro.inc"
+%include "vectors.inc"
 
 
 ;======================================================================
@@ -20,11 +21,10 @@ org 0
 ;======================================================================
 
 ; important segments
-cseg            equ 0F000h
+cseg_init       equ 0F000h  ; CS
+cseg            equ 00000h  ; CS after relocation
 dseg            equ 01000h
 sseg            equ 00000h
-rseg            equ 00000h  ; relocation segment
-useg            equ 01000h  ; user segment
 
 ; I/O addresses should be even numbers only
 ticks_ctrl      equ 04h
@@ -38,123 +38,188 @@ uart_data       equ 22h
 ; 0xf0100 - monitor start
 ;======================================================================
 
-%include "vectors.inc"
-
 init:
+; clear interrupts
     cli
     cld
 
 ; ensure UART has interrupts disabled
     xor al,al
     out uart_ctrl,al
-    
+
 ; copy ROM to RAM
 ; don't use the stack yet as the ROM copy will overwrite any stored data
-    mov ax,cseg
+    mov ax,cseg_init
     mov ds,ax
     xor si,si
 
-    mov ax,rseg
+    mov ax,cseg
     mov es,ax
     xor di,di
 
     mov cx,08000h
 
-.rom_copy:
+.copy_rom:
     movsw
-    loop .rom_copy
+    loop .copy_rom
 
-    jmp rseg:reloc
+    jmp cseg:relocate
 
-reloc:
-; toggle ROM -> RAM
-    out mem_toggle, al      ; value of al doesn't matter
+
+;======================================================================
+; 0x00xxx - monitor relocation
+;======================================================================
+
+relocate:
+; initialise DS
+    mov ax,dseg
+    mov ds,ax
 
 ; initialise SS/SP
     mov ax,sseg
     mov ss,ax
     mov sp,0FFFFh
 
-; emable interrupts
+; toggle ROM -> RAM
+    out mem_toggle, al      ; value of al doesn't matter
+
+; enable interrupts
     sti
 
-; initialise DS
-    mov ax,dseg
-    mov ds,ax
+
+;======================================================================
+; welcome
+;======================================================================
 
 ; welcome
     print mesg_welcome
+    print mesg_copyright
+    print mesg_build
 
-menu:
-    print mesg_menu
+
+;======================================================================
+; menu
+;======================================================================
+
+; prompt
+prompt:
+    print mesg_prompt
+
     call read_chr
-    push ax
-    call print_chr
-
-    mov al,0Ah
-    call print_chr
-    mov al,0Ah
-    call print_chr
-    pop ax
-
     cmp al,'d'
-    je dump
-    cmp al,'D'
-    je dump
+    je  menu_dump
     cmp al,'h'
-    je halt
-    cmp al,'H'
-    je halt
-    cmp al,'p'
-    je pitest
-    cmp al,'P'
-    je pitest
+    je  menu_help
+    cmp al,'i'
+    je  menu_inp
+    cmp al,'j'
+    je  menu_jump
+    cmp al,'l'
+    je  menu_load
+    cmp al,'o'
+    je  menu_out
     cmp al,'r'
-    je run
-    cmp al,'R'
-    je run
-    cmp al,'t'
-    je ticks
-    cmp al,'T'
-    je ticks
-    cmp al,'u'
-    je upload
-    cmp al,'U'
-    je upload
+    je  menu_dump
+    cmp al,'x'
+    je  menu_exe
+    jmp prompt
 
-    print mesg_unknown_cmd
-
-    jmp menu
-
-
-upload:
-; prep upload
-    print mesg_upload
-
-; initialise ES
-    mov ax,cseg
-    mov es,ax
-
-; read hex file
-    call read_hex_file
-    print mesg_upload_done
-
-    jmp menu
-
-dump:
+menu_dump:
 ; dump memory
-    print mesg_memdump
-    mov ax,cseg
-    mov ds,ax
-    mov si,0
-    mov cx,384
+    print mesg_dump
+
+    push ds
+    push bx
+    push si
+
+    call read_chr           ; space
+
+    call read_hex           ; get segment
+    mov bh,al
+    call read_hex           ; get segment
+    mov bl,al
+    mov ds,bx
+
+    call read_chr           ; colon
+
+    call read_hex           ; get offset
+    mov bh,al
+    call read_hex           ; get offset
+    mov bl,al
+    mov si,bx
+
+    mov cx,256
     call mem_dump
 
-    jmp menu
+    pop si
+    pop bx
+    pop ds
+    jmp prompt
 
-run:
-; reset
-    print mesg_restart
+menu_help:
+; help
+    print mesg_help
+    jmp prompt
+
+menu_inp:
+; inp pp
+    print mesg_inp
+    push dx
+    xor dx,dx
+    call read_hex           ; get port
+    mov dl,al
+    in al,dx                ; INP
+    call print_hex
+    pop dx
+    jmp prompt
+
+menu_jump:
+; jump
+    print mesg_jump
+
+    call read_chr           ; space
+    call read_hex           ; get segment
+    mov bh,al
+    call read_hex           ; get segment
+    mov bl,al
+    mov [ start_segment ], bx
+
+    call read_chr           ; colon
+
+    call read_hex           ; get offset
+    mov bh,al
+    call read_hex           ; get offset
+    mov bl,al
+    mov [ start_address ], bx
+; indirect far jump to CS:IP
+    jmp far [ start_address ]
+
+menu_load:
+; load intel hex file
+    print mesg_load
+    call read_hex_file
+    print mesg_load_done
+    jmp prompt
+
+menu_out:
+; out pp xx
+    print mesg_out
+    push dx
+
+    xor dx,dx
+    call read_hex           ; get port
+    mov dl,al
+    call read_chr           ; space
+    call read_hex           ; get hex
+    out dx,al
+
+    pop dx
+    jmp prompt
+
+menu_exe:
+; execute
+    print mesg_out
+
     xor ax,ax
     mov ds,ax
     mov es,ax
@@ -166,116 +231,14 @@ run:
 ; indirect far jump to CS:IP
     jmp far [ start_address ]
 
-ticks:
-; toggle the ticks timer on/off
-    print mesg_ticks
-
-    in al,ticks_ctrl
-    not al
-    out ticks_ctrl,al
-
-    jmp menu
-
-pitest:
-    print mesg_pi
-    mov al,1
-    mov [ piactive ],al
-    out uart_ctrl,al
-
-.pitest1:
-    mov al,[ piactive ]
-    cmp al,0
-    jne .pitest1
-
-    xor al,al
-    out uart_ctrl,al
-
-    mov al,0Ah
-    call print_chr
-    call print_chr
-    
-    jmp menu
-
-piactive:
-    db 0
-    
-halt:
-    hlt
-    jmp menu
-
-
-led_blink:
-    call set_leds
-
-    mov ax,500
-    call delay_ms
-
-    xor ax,ax
-    call set_leds
-
-    ret
-
 
 ;======================================================================
-; intr_debug
+; intr_dummy
 ;======================================================================
 
 int_dummy:
     iret
 
-int_irq0:
-    push ax
-    mov al,00000001b
-    call led_blink
-    pop ax
-    iret
-
-int_irq1:
-    push ax
-    mov al,00000010b
-    call led_blink
-    pop ax
-    iret
-
-int_irq2:
-    push ax
-    mov al,00000100b
-    call led_blink
-    pop ax
-    iret
-
-int_irq3:
-    push ax
-    mov al,00001000b
-    call led_blink
-    pop ax
-    iret
-
-int_piuart:
-    push ax
-    in al,uart_data
-    cmp al,01Bh                 ; ESC
-    jne .int_piuart1
-    xor al,al
-    out uart_ctrl,al            ; disable UART interrupts
-    mov [ piactive ],al
-.int_piuart1:
-    out leds_data,al
-    out uart_data,al
-    pop ax
-    iret
-
-int_ticks:
-    push ax
-    mov ax,[ ticks_count ]
-    inc ax
-    mov [ ticks_count ],ax
-    call set_leds
-    pop ax
-    iret
-
-ticks_count:
-    dw 0
 
 ;======================================================================
 ; includes
@@ -293,10 +256,13 @@ ticks_count:
 ; intel hex file start address
 ;======================================================================
 
+TIMES 65536-64-($-$$)       db 0FFh
+
 start_address:
-    dw 0
+    dw 00h
 start_segment:
-    dw 0
+    dw 00h
+
 
 ;======================================================================
 ; reset code (called on CPU reset)
@@ -305,9 +271,12 @@ start_segment:
 TIMES 65536-16-($-$$)       db 0FFh
 
 reset:
-    jmp cseg:init
+    jmp cseg_init:init
 
-build:
-    db __DATE__
-    db 0
+
+;======================================================================
+; filler
+;======================================================================
+
+TIMES 65536-($-$$)          db 0FFh
 
