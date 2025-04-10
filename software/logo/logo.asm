5706efe1d6485d9bb4be59cf289e83144754ea4a
Dylan Hall <dylan@deedums.com>
2025-04-11 00:20:38 +1200

    Updates to match video mode changes

--- software/logo/logo.asm
+++ software/logo/logo.asm
@@ -25,12 +25,8 @@ init:
     mov sp,0FFFFh
 
     ; set the video mode
-    mov al,1
-    out VC_MODE,al
-
-    xor al,al
-    out VC_HIGH_ADDR,al
-    out VC_LOW_ADDR,al
+    mov al,11h              ; mode 1 with auto increment on write
+    out VC_CTRL,al
 
     cld
     mov ax,cs
@@ -46,6 +42,38 @@ init:
     ; setup tune pointer
     mov si,imperial
     call tune
+
+    ; set the video mode
+    mov al,12h              ; mode 2 with auto increment on write
+    out VC_CTRL,al
+
+    ; bh == Y coord
+    ; bl == X coord
+
+    xor bx,bx
+
+.loop:
+    mov al,bh
+    out VC_HIGH_ADDR,al
+    inc bh
+
+    mov al,bl
+    out VC_LOW_ADDR,al
+    inc bl
+    inc bl
+
+    mov al,0Fh
+    out VC_DATA,al
+
+.wait_for_vsync:
+    in al,VC_CTRL
+    test al,80h
+    jz .wait_for_vsync
+
+    ; swap buffers by writing video mode again
+    out VC_CTRL,al
+
+    jmp .loop
 
     hlt
 
